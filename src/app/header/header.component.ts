e98e5bd716f2532f0b4899f196550fa71e653682
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2016-11-05 04:30:44 +0100

    feat(header): add 2-way databinding + enter action, link with todo store

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { TodoStore } from '../services/todo.store';
 
 @Component({
     selector: 'header',
+    providers: [TodoStore],
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
     title = 'todos';
+
+    todoStore: TodoStore;
+    @Input() newTodoText: string = '';
+
+    constructor(todoStore: TodoStore) {
+		this.todoStore = todoStore;
+	}
+
+    addTodo() {
+		if (this.newTodoText.trim().length) {
+            this.todoStore.add(this.newTodoText);
+			this.newTodoText = '';
+		}
+	}
 }
--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { HeaderComponent } from './header.component';
 
 @NgModule({
+    imports: [
+        FormsModule
+    ],
     declarations: [
         HeaderComponent
     ],
