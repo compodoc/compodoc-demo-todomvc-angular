967fdf2b8e44118d21a4835feed972c9aa8c9351
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2016-11-06 08:10:29 +0100

    feat(footer): add fx

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
 
+import { TodoStore } from '../services/todo.store';
+
 @Component({
     selector: 'footer',
+    providers: [TodoStore],
     templateUrl: './footer.component.html'
 })
 export class FooterComponent {
+    /**
+     * Local reference of TodoStore
+     */
+    todoStore: TodoStore;
 
+    constructor(todoStore: TodoStore) {
+		this.todoStore = todoStore;
+	}
+
+    removeCompleted() {
+		this.todoStore.removeCompleted();
+	}
 }
--- src/app/footer/footer.module.ts
+++ src/app/footer/footer.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { FooterComponent } from './footer.component';
 
 @NgModule({
+    imports: [
+        BrowserModule
+    ],
     declarations: [
         FooterComponent
     ],
