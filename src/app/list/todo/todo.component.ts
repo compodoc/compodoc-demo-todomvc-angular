eabcc8e4bac66d7924a899a36ef61c87b6d3f9ce
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2016-11-06 07:53:08 +0100

    feat(todo): remove

--- src/app/list/todo/todo.component.ts
+++ src/app/list/todo/todo.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 
 import { Todo } from '../../models/todo.model';
 
+import { TodoStore } from '../../services/todo.store';
+
 @Component({
     selector: 'todo',
     templateUrl: './todo.component.html'
@@ -12,7 +14,16 @@ export class TodoComponent {
      */
     @Input() todo: Todo;
 
-    constructor() {
-        
-    }
+    /**
+     * Local reference of TodoStore
+     */
+    todoStore: TodoStore;
+
+    constructor(todoStore: TodoStore) {
+		this.todoStore = todoStore;
+	}
+
+    remove(todo: Todo){
+		this.todoStore.remove(todo);
+	}
 }
