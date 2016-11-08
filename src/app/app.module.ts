ff776354365acf386c5869bc182b07c07570c60e
vogloblinsky <vincent.ogloblinsky@gmail.com>
2016-11-08 13:43:41 +0100

    feat(app): routing added, home and about pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes }   from '@angular/router';
 
 import { HeaderModule } from './header/';
+
+import { AboutModule, AboutComponent } from './about/';
+
+import { HomeModule, HomeComponent } from './home/';
 
 import { ListModule } from './list/';
 
@@ -13,6 +18,11 @@ import { AppComponent } from './app.component';
 
 import { TodoStore } from './shared/services/todo.store';
 
+const APP_ROUTES: Routes = [
+    { path: 'about', component: AboutComponent },
+    { path: '', component: HomeComponent}
+];
+
 /**
  * The bootstrapper module
  */
@@ -21,13 +31,10 @@ import { TodoStore } from './shared/services/todo.store';
         AppComponent
     ],
     imports: [
-        BrowserModule,
-        FormsModule,
-        HttpModule,
+        AboutModule,
+        HomeModule,
 
-        HeaderModule,
-        ListModule,
-        FooterModule
+        RouterModule.forRoot(APP_ROUTES)
     ],
     providers: [
         TodoStore
