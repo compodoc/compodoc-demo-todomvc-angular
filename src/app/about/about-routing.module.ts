2fd2155cd3c8cc892e05fbc26adeec38988b15dc
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2016-12-23 07:29:24 +0100

    feat(app): add child routes for compodoc generation

--- src/app/about/about-routing.module.ts
+++ src/app/about/about-routing.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AboutComponent } from './about.component';
+import { TodoMVCComponent } from './todomvc/todomvc.component';
+import { CompodocComponent } from './compodoc/compodoc.component';
 
 const APP_ROUTES: Routes = [
-    { path: '', component: AboutComponent }
+    {
+        path: '', component: AboutComponent,
+        children: [
+            { path: '', redirectTo: 'todomvc', pathMatch: 'full' },
+            { path: 'todomvc', component: TodoMVCComponent },
+            { path: 'compodoc', component: CompodocComponent }
+        ]
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(APP_ROUTES)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(APP_ROUTES)],
+    exports: [RouterModule],
+    declarations: [
+        TodoMVCComponent,
+        CompodocComponent
+    ]
 })
-export class AboutRoutingModule {}
+export class AboutRoutingModule { }
