6eeec4e72877dc697d30cc9b917cbb670a570343
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2017-07-16 15:14:26 +0200

    Markdowns

--- src/app/shared/interfaces/interfaces.ts
+++ src/app/shared/interfaces/interfaces.ts
@@ -44,24 +44,3 @@ interface SearchFunc {
 interface StringArray {
     [index: number]: string;
 }
-
-/**
- * A class interface just for documentation purpose
- * ```typescript
- * class Clock implements ClockInterface {
- *     currentTime: Date;
- *     constructor(h: number, m: number) { }
- * }
- * ```
- */
-interface ClockInterface {
-    /**
-     * The current time
-     * @type {Date}
-     */
-    currentTime: Date;
-    /**
-     * A simple reset method
-     */
-    reset(): void;
-}
