f438e577b99200360a239ecc72f63de71a1851b4
vogloblinsky <vincent.ogloblinsky@gmail.com>
2020-10-08 10:23:24 +0000

    Deploying to gh-pages from  @ 10ebf65e0d13e41c880537d6bb7be8d7bbd0f4d4 🚀

--- js/menu-wc.js
+++ js/menu-wc.js
@@ -16,7 +16,7 @@ customElements.define('compodoc-menu', class extends HTMLElement {
         <nav>
             <ul class="list">
                 <li class="title">
-                    <a href="index.html" data-type="index-link">TodoMVC Angular documentatione</a>
+                    <a href="index.html" data-type="index-link">TodoMVC Angular documentation</a>
                 </li>
 
                 <li class="divider"></li>
