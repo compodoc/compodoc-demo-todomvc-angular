d4a6487582b2bfa713228b5533ccb776fc00d0bd
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2021-07-12 16:44:25 +0200

    feat(routing): routing syntax

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutModule } from './about';
 
 import { APP_ENUMS } from './app-routes.enum';
 
 enum APP_ENUM {
-    home = 'home'
+    home = 'home',
 }
 
 export const APP_ROUTES: Routes = [
     {
         path: 'about',
-        loadChildren: () =>
-            import('./about/about.module').then(m => m.AboutModule)
+        loadChildren: (): Promise<AboutModule> =>
+            import('./about/about.module').then((m) => m.AboutModule),
     },
     //{ path: 'about', loadChildren: './about/about.module#AboutModule' },
     { path: '', redirectTo: APP_ENUMS.home, pathMatch: 'full' },
-    { path: '**', redirectTo: APP_ENUM.home, pathMatch: 'full' }
+    { path: '**', redirectTo: APP_ENUM.home, pathMatch: 'full' },
 ];
 
 @NgModule({
     imports: [RouterModule.forRoot(APP_ROUTES)],
-    exports: [RouterModule]
+    exports: [RouterModule],
 })
 export class AppRoutingModule {}
